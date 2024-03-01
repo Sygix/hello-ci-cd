7976c64144a68bf62d6749ee591b0ac2c01a22e4
Boudzin <hugolethellier@orange.fr>
2024-03-01 11:46:43 +0100

    Rename

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const Express = require('express');
+const Express = require("express");
 
 const app = new Express();
 const port = 1234;
 
 app.get("*", (req, res) => {
-    res.send("Hello Sciences-U !\n");
+  res.send("Hello 4INFO-D !\n");
 });
 
 app.listen(port, () => {
-    console.log('Server is running on port ' + port + '...');
+  console.log("Server is running on port " + port + "...");
 });
