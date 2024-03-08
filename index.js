1c35475e0559d6992a844a3d0636e48387a0413f
Sygix <17769298+Sygix@users.noreply.github.com>
2024-03-08 10:38:27 +0100

    Fix eslint rules and remove semicolons

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const Express = require('express');
+const Express = require('express')
 
-const app = new Express();
-const port = 1234;
+const app = new Express()
+const port = 1234
 
 app.get('*', (req, res) => {
-  res.send('Hello 4INFO-D !\n');
-});
+  res.send('Hello 4INFO-D !\n')
+})
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
-  console.log(`Server is running on port ${port}...`);
-});
+  console.log(`Server is running on port ${port}...`)
+})
